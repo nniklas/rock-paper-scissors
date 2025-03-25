051eda03482ea195dd363d5249ce2f5f95d57b42
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-25 16:19:58 +0100

    add function getComputerChoice that returns rock, paper or scissors as strings

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
 console.log("This is the Rock Paper Scissors game part 1 for ToP")
+
+
+// function that randomly returns "rock", "paper" or "scissors" as strings
+function getComputerChoice(){
+    let randomNumber = Math.random();
+    let computerChoice = "";
+    
+    if (randomNumber <= 0.33) {
+        computerChoice = "rock"
+    }   else if (randomNumber > 0.33 && randomNumber < 0.66){
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
