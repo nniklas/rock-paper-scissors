a23ef67c38a2d40c51eb08e28eaea0419a85d774
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-26 12:35:52 +0100

    adjust humanInput function to accept strings

--- java.js
+++ java.js
@@ -4,16 +4,16 @@ console.log("This is the Rock Paper Scissors game part 1 for ToP")
 // function that randomly returns "rock", "paper" or "scissors" as strings
 function getComputerChoice(){
     let randomNumber = Math.random();
-    let computerChoice = "";
+    let computerSelection = "";
     
     if (randomNumber <= 0.33) {
-        computerChoice = "rock"
+        computerSelection = "rock"
     }   else if (randomNumber > 0.33 && randomNumber < 0.66){
-        computerChoice = "paper"
+        computerSelection = "paper"
     } else {
-        computerChoice = "scissors"
+        computerSelection = "scissors"
     }
-    return computerChoice;
+    return computerSelection;
 }
 
 // log computer choice
@@ -22,17 +22,17 @@ console.log(getComputerChoice());
 // function that get human choice, return one of the valid choices based on user inputs
 function getHumanChoice(){
     let message = "Choose rock, paper or scissors, type in your answer using numbers only. Rock=1, paper = 2, scissors = 3";
-    let humanInput = +prompt(message);
-    let humanChoice = "poo";
+    let humanInput = prompt(message).toLowerCase();
+    let humanSelection = "poo";
 
-    if (humanInput==1){
-        humanChoice = "rock";
-    } else if (humanInput == 2){
-        humanChoice = "paper"
-    } else if (humanInput == 3){
-        humanChoice = "scissors"
+    if (humanInput=="rock"){
+        humanSelection = "rock";
+    } else if (humanInput == "paper"){
+        humanSelection = "paper"
+    } else if (humanInput == "scissors"){
+        humanSelection = "scissors"
     }
-    return humanChoice
+    return humanSelection
 }
 
 // log humanchoice
@@ -43,6 +43,13 @@ let humanScore = 0;
 let computerScore = 0;
 
 // logic to play a single round
+// function that takes the human and computer player choices as arguments, 
+// plays a single round, increments the round winner’s score and logs a winner announcement
+
+function playRound (humanChoice, computerChoice){
+
+}
+
 
 
 //logic to play entire game
